a1277df4b8109520da2fc9d686dbd899412d3f4e
Дамјан Георгиевски <gdamjan@gmail.com>
2024-01-03 02:39:38 +0100

    pass self to all outputs

--- flake.nix
+++ flake.nix
@@ -4,19 +4,20 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs = inputs @ { self, nixpkgs }:
-    let system = "x86_64-linux"; in {
+    let
+      system = "x86_64-linux";
+      specialArgs = { inherit self inputs; };
+    in {
       nixosConfigurations = {
         nix-vm = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit self inputs; };
+          inherit system specialArgs;
           modules = [
             ./common.nix
             ./nix-vm/configuration.nix
           ];
         };
         nix-container = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit inputs; };
+          inherit system specialArgs;
           modules = [
             ./common.nix
             ./nix-container/configuration.nix
