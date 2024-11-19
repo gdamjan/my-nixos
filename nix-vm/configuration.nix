b54207b01fd5d5e9a507b4032b746d7252e0baed
Дамјан Георгиевски <gdamjan@gmail.com>
2024-11-19 04:00:24 +0100

    simplify nix-vm config
    use the gpt-auto mount generator, so no need to specify filesystems manually
    added emergency shell password - helps debug initrd

--- nix-vm/configuration.nix
+++ nix-vm/configuration.nix
@@ -1,19 +1,27 @@
-{ config, pkgs, ... }:
+{ config, pkgs, modulesPath, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    [
+      (modulesPath + "/profiles/qemu-guest.nix")
     ];
+  networking.hostName = "nix-vm";
+  networking.interfaces.enp3s0.useDHCP = true;
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.supportedFilesystems = [ "ext4" ];
+
+  boot.initrd.systemd.emergencyAccess = "$y$j9T$IdTz7H.u4VSAfnlGs/6381$VHgm/PZOirdvcHbwPDYCkyxqKkMzIVTczagNp4OuSa1";
+
   boot.loader.systemd-boot.enable = true;
   boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.root = "gpt-auto";
+  boot.initrd.systemd.repart.enable  = true;
+  boot.initrd.systemd.repart.device = "/dev/sda";
   boot.kernelParams = [
     "console=ttyS0,115200"
     "console=tty1"
-    "rootflags=nodev"
   ];
-
-  networking.hostName = "nix-vm";
-  networking.interfaces.enp3s0.useDHCP = true;
 }
