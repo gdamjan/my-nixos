b3796a125e6c9c5449423eb3184c6be4841c99af
Дамјан Георгиевски <gdamjan@gmail.com>
2024-11-19 04:03:15 +0100

    bump to nixos-24.11 20241116

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "NixOS configuration";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
 
   outputs = inputs @ { self, nixpkgs }:
     let
