7fcb17681ad583a1b7ff40ebcf06c23114255640
Дамјан Георгиевски <gdamjan@gmail.com>
2024-12-30 23:31:36 +0000

    nixosaurus: createHome=false
    I have the home GPT partition mounted by systemd-gpt-auto-generator
    but that would fail if nixos previously creates /home/damjan thus making
    /home non-empty

--- nixosaurus/configuration.nix
+++ nixosaurus/configuration.nix
@@ -23,6 +23,7 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.damjan = {
+    createHome = false;
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
