57b96648b722c17dd3e7a55b1d59d4d97a09f586
Дамјан Георгиевски <gdamjan@gmail.com>
2024-12-30 23:44:31 +0000

    nixosaurus: add rqbit service

--- nixosaurus/services.nix
+++ nixosaurus/services.nix
@@ -3,4 +3,28 @@
   services.resolved.enable = true;
   services.openssh.enable = true;
 
+  systemd.services.rqbit = {
+    enable = true;
+    description = "A streaming bittorrent client in Rust";
+    unitConfig = {
+      After = "network-online.target";
+      Wants = "network-online.target";
+    };
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.rqbit}/bin/rqbit server start $DOWNLOAD_DIR";
+      DynamicUser = true;
+      StateDirectory = "rqbit";
+      CacheDirectory = "rqbit";
+    };
+    environment = {
+      XDG_CACHE_HOME = "/var/cache/rqbit";
+      XDG_DATA_HOME = "/var/lib/rqbit";
+      DOWNLOAD_DIR = "/var/tmp";
+      RQBIT_HTTP_API_LISTEN_ADDR = "0.0.0.0:3030";
+      RQBIT_UPNP_SERVER_ENABLE = "true";
+      RQBIT_LOG_LEVEL_CONSOLE = "warn";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 }
