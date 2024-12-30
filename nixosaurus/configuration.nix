c9da67a9d4c925f0fe8a037e19da6c7f29ed57b5
Дамјан Георгиевски <gdamjan@gmail.com>
2024-12-30 23:43:50 +0000

    nixosaurus: shuffle packages around
    systemPackages are more useful

--- nixosaurus/configuration.nix
+++ nixosaurus/configuration.nix
@@ -28,16 +28,17 @@
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       firefox
-      git
-      tmux
     ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeHvI5nYLPpBaMTRny9XFouOZSrwkYGSUWFd8i1OP1Q damjan@lokutis" ];
   };
 
   environment.systemPackages = with pkgs; [
+    git
     vim
     wget
     nix-tree
+    lm_sensors
+    tmux
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
